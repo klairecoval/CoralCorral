b66c4ac767b6f9f7184f0d18937e43c7f7051380
Claire Koval <klairecoval@gmail.com>
2019-10-03 08:44:25 -0400

    start info

--- src/source.js
+++ src/source.js
@@ -1,7 +1,5 @@
 var modal = document.getElementById("myModal");
-
 var btn = document.getElementById("myBtn");
-
 var span = document.getElementsByClassName("close")[0];
 
 btn.onclick = function() {
